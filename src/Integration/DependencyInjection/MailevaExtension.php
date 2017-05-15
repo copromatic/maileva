79e02cf41a0871adda25fef4737dedede01809a7
Yannis Touili <gowtcha@gmail.com>
2017-05-15 17:57:00 +0200

    rename extension integration for symfony

--- src/Integration/DependencyInjection/MailevaExtension.php
+++ src/Integration/DependencyInjection/MailevaExtension.php
@@ -2,10 +2,10 @@
 
 namespace Maileva\Integration\DependencyInjection;
 
-use Maileva\DependencyInjection\MailevaExtension;
+use Maileva\DependencyInjection\MailevaExtension as MailevaExtensionVendor;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
-class MailevaBundleExtension extends MailevaExtension {
+class MailevaExtension extends MailevaExtensionVendor {
     public function load(array $configs, ContainerBuilder $container) {
         parent::load($configs, $container);
     }
