5dd453a402670e86fc3ad83aebfc6fbb7d79a6f9
Gaetan Hautecoeur <gaetan.hautecoeur@gmail.com>
2017-11-07 11:51:34 +0100

    email  for notification not required

--- src/DependencyInjection/MailevaExtension.php
+++ src/DependencyInjection/MailevaExtension.php
@@ -36,7 +36,10 @@ class MailevaExtension implements ExtensionInterface {
             $config['rate']['fold']['recommandear'], $config['rate']['weight'],
             $config['A4paperweight']
         ]);
-        $defApi->addMethodCall('setEmailNotification', [$config['email_notification']]);
+        
+        if(isset($config['email_notification']) && $config['email_notification'] != ''){
+            $defApi->addMethodCall('setEmailNotification', [$config['email_notification']]);
+        }
 
         //$defApi->addMethodCall('setLogger', $container->getDefinition('logger'));
     }
